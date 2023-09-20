2676ee50787a9dab5596b8c4e1b7c8c7dd3a8225
MushiTea <94145946+MushiTea@users.noreply.github.com>
2023-09-20 07:39:03 -0500

    Gecko wheel flipper prototype idea

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/KaidaProto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/KaidaProto.java
@@ -3,33 +3,32 @@ package org.firstinspires.ftc.teamcode.MechanismTemplates;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Config
 @TeleOp(name = "KaidaProto")
-public class MechanismTest extends OpMode {
-    public static double power = 0.0;
-    private DcMotorEx left, right;
+public class KaidaProto extends OpMode{
+    public static double power = 1;
+    private DcMotorEx left; //right;
 
     @Override
     public void init(){
         left = (DcMotorEx) hardwareMap.dcMotor.get("L");
-        right = (DcMotorEx) hardwareMap.dcMotor.get("R");
+        //right = (DcMotorEx) hardwareMap.dcMotor.get("R");
 
         left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        right.setDirection(DcMotorSimple.Direction.REVERSE);
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
 
     @Override
     public void loop(){
         left.setPower(power);
-        left.setPower(power);
+        //right.setPower(power);
     }
 
 }
