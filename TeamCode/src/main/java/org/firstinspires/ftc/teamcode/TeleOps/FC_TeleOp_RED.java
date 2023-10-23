bbad3116110ea2ebf7ad1e8af0fe826ef2a159ff
ridhima <ridhima.d.gupta@gmail.com>
2023-10-23 18:23:02 -0300

    working field centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/FC_TeleOp_RED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/FC_TeleOp_RED.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
-public class FC_TeleOp extends LinearOpMode {
+public class FC_TeleOp_RED extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
@@ -50,9 +50,9 @@ public class FC_TeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = -gamepad1.right_stick_x;
+            double y = gamepad1.left_stick_y; // Remember, this is reversed!//i reversed it again
+            double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
 
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
@@ -70,10 +70,10 @@ public class FC_TeleOp extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double denominator = -(Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1));
             double frontLeftPower = (rotY + rotX + rx) / denominator;
             double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double frontRightPower =(rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
 
             motorFrontLeft.setPower(frontLeftPower);
