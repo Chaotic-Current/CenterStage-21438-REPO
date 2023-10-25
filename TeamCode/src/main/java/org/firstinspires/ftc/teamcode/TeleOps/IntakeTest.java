883e0b7f26338579dccfedb5a7fd486f51cfcff3
MushiTea <94145946+MushiTea@users.noreply.github.com>
2023-10-25 17:40:55 -0500

    intake working 10/25/23 yippeee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/IntakeTest.java
@@ -14,99 +14,48 @@ public class IntakeTest extends OpMode {
     //"MC ABHI IS ON THE REPO!!!"
 
     // Declaring intake motors
-    private DcMotorEx intake;
+    private DcMotorEx intake; // pin 0
 
-    private Servo left, right;
+    private Servo left, right; // left -> 0, right -> 1
+    public static double leftPower = 0.74;//moving a dist on 0.06
+    public static double rightPower = 0.26;
 
     @Override
     public void init() {
-        intake = (DcMotorEx) hardwareMap.dcMotor.get("FL"); // Pin 2 -> pin 3
+        intake = (DcMotorEx) hardwareMap.dcMotor.get("intake");
+        left = hardwareMap.get(Servo.class, "left");
+        right = hardwareMap.get(Servo.class, "right");
 
-        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Running without an encoder allows us to plug in a raw value rather than one that is proportional
+        left.setPosition(0.8); // left start pos
+        right.setPosition(0.2); // right start pos
+
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // Reverse motors
         intake.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        // We want to check this every time the loop runs
-        drive();
-    }
-
-    public void drive(){
-        double y = reducingDeadzone(gamepad1.left_stick_y); // Remember, this is reversed!
-        double x = reducingDeadzone(-gamepad1.left_stick_x);
-        boolean precisionToggle = gamepad1.right_trigger > 0.1;
-        double rx = reducingDeadzone(-gamepad1.right_stick_x * 0.75);
-        if (precisionToggle) {
-            rx *= TURN_PRECESION;
+        if(gamepad1.right_trigger > 0.1) {
+            intake.setPower(1);
+        }else{
+            intake.setPower(0);
+        }
+        if(gamepad1.y){
+            telemetry.addData("yea", 1);
+            left.setPosition(leftPower);
+            right.setPosition(rightPower);
+        }
+        if(gamepad1.b){
+            telemetry.addData("idk", 1);
+            left.setPosition(0.8);
+            right.setPosition(0.2);
         }
 
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio, but only when
-        // at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-
-        // Calculate the mecanum motor powers
-        double frontLeftPower = (y + x + 2 * rx) / denominator;
-        double backLeftPower = (y - x + 2 * rx) / denominator;
-        double frontRightPower = (y - x - 2 * rx) / denominator;
-        double backRightPower = (y + x - 2 * rx) / denominator;
-
-        // Cube the motor powers
-        frontLeftPower = Math.pow(frontLeftPower, 3);
-        frontRightPower = Math.pow(frontRightPower, 3);
-        backLeftPower = Math.pow(backLeftPower, 3);
-        backRightPower = Math.pow(backRightPower, 3);
-
-        // Calculate the maximum value of all the motor powers
-        // The argument here is just an array separated into different lines
-        double maxValue = getMax(new double[]{
-                frontLeftPower,
-                frontRightPower,
-                backLeftPower,
-                backRightPower
-        });
-        // Resize the motor power values
-        if (maxValue > 1) {
-            frontLeftPower /= maxValue;
-            frontRightPower /= maxValue;
-            backLeftPower /= maxValue;
-            backRightPower /= maxValue;
-        }
-
-        telemetry.addData("frontLeftPow", frontLeftPower);
-        telemetry.addData("frontRightPow", frontRightPower);
-        telemetry.addData("backLeftPow", backLeftPower);
-        telemetry.addData("backRightPow", backRightPower);
         telemetry.update();
 
-        if (precisionToggle) {
-            motorFrontLeft.setPower(frontLeftPower * PRECISIONREDUCTION);
-            motorBackLeft.setPower(backLeftPower * PRECISIONREDUCTION);
-            motorFrontRight.setPower(frontRightPower * PRECISIONREDUCTION);
-            motorBackRight.setPower(backRightPower * PRECISIONREDUCTION);
-        } else {
-            motorFrontLeft.setPower(frontLeftPower);
-            motorBackLeft.setPower(backLeftPower);
-            motorFrontRight.setPower(frontRightPower);
-            motorBackRight.setPower(backRightPower);
-        }
-
-    }// end of drive()
-
-    public double reducingDeadzone(double x) {
-        if (x == 0) {
-            return 0;
-        } else if (0 < x && 0.25 > x) {
-            return 0.25;
-        } else if (0 > x && x > -0.25) {
-            return -0.25;
-        }
-        return x;
     }
 }
 
