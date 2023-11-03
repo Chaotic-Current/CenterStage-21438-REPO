4920a94b871d20b5a9cfe7835f8eedc6120767a4
MushiTea <94145946+MushiTea@users.noreply.github.com>
2023-11-03 08:55:27 -0500

    OtherSlide test smh srinirekdidn't push smhsmsh imoimo smh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/OtherSlideTest.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.MechanismTemplates;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+@TeleOp(name = "OtherSlideTest")
+public class OtherSlideTest extends OpMode {
+    //"MC ABHI IS ON THE REPO!!!"
+
+    // Declaring intake motors
+    private DcMotorEx SR, SL; // pin 0
+
+    @Override
+    public void init() {
+        SR = (DcMotorEx) hardwareMap.dcMotor.get("SR");
+        SL = (DcMotorEx) hardwareMap.dcMotor.get("SL");
+
+        SR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        SL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        SR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        SL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        SR.setDirection(DcMotorSimple.Direction.REVERSE);
+        SL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.right_trigger > 0.2){
+            SR.setPower(0.3);
+            SR.setPower(0.3);
+        }else if(gamepad1.left_trigger > 0.2){
+            SR.setPower(-0.3);
+            SL.setPower(-0.3);
+        }
+        else{
+            SR.setPower(0);
+            SL.setPower(0);
+        }
+
+
+    }
+}
+
+
+
