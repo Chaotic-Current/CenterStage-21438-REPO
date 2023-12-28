3872edb749e3c26ec302ae229db4cd2b2bb87c52
MushiTea <94145946+MushiTea@users.noreply.github.com>
2023-12-27 22:20:31 -0600

    webhook pwease work (´▽`ʃ♡ƪ) pweeeease

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueCloseSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueCloseSide.java
@@ -30,7 +30,7 @@ import org.openftc.easyopencv.OpenCvWebcam;
 @Config
 @Autonomous
 public class BlueCloseSide extends LinearOpMode {
-    // edit for webhook testing 1
+    // edit for webhook testing 2
     private SampleMecanumDrive drive;
     private ElapsedTime timer = new ElapsedTime();
     private OpenCvWebcam frontCam, backCam;
